6706a7ab281de49e9863156812c0352f83c95d50
marco-antonio-aguilar-gallardo <marco-antonio@outlook.com>
2019-10-08 22:05:18 -0500

    The firs catkin_make was lanched

---
+++ ROS/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/marco/Tesis/ROS/src
+# Build directory: /home/marco/Tesis/ROS/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("video_stream_opencv")
