ca0d737ee8747b82d553cb41c6f8ffc599aa08f6
marco-antonio-aguilar-gallardo <marco-antonio@outlook.com>
2019-11-20 21:06:08 -0600

    updated ROS

--- ROS/build/rosserial_python/CTestTestfile.cmake
+++ ROS/build/rosserial_python/CTestTestfile.cmake
@@ -1,9 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /home/marco/Tesis/ROS/src
-# Build directory: /home/marco/Tesis/ROS/build
+# Source directory: /home/marco/Tesis/ROS/src/rosserial_python
+# Build directory: /home/marco/Tesis/ROS/build/rosserial_python
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("rosserial_python")
-subdirs("video_stream_opencv")
