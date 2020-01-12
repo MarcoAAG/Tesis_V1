842e9e3ce0816a07b3158a8488d7848e7d4f444f
MarcoAAG <marco-antonioa@outlook.com>
2020-01-12 13:32:05 -0600

    catkin make camera calibration

--- ROS/build/CTestTestfile.cmake
+++ ROS/build/CTestTestfile.cmake
@@ -5,5 +5,13 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
+subdirs("image_pipeline/image_pipeline")
 subdirs("rosserial_python")
+subdirs("image_pipeline/camera_calibration")
+subdirs("image_pipeline/image_proc")
+subdirs("image_pipeline/image_publisher")
 subdirs("ocam")
+subdirs("image_pipeline/image_view")
+subdirs("image_pipeline/stereo_image_proc")
+subdirs("image_pipeline/depth_image_proc")
+subdirs("image_pipeline/image_rotate")
