d501f4c6548db4d7df029084c02fb9e1e6729ec1
MarcoAAG <marco-antonioa@outlook.com>
2020-02-06 23:23:19 -0600

    created a hercules project

---
+++ microcontrolador/GM2804/Debug/source/HL_errata_SSWF021_45.d
@@ -0,0 +1,9 @@
+# FIXED
+
+source/HL_errata_SSWF021_45.obj: ../source/HL_errata_SSWF021_45.c
+source/HL_errata_SSWF021_45.obj: /home/marco/Tesis/microcontrolador/GM2804/include/HL_errata_SSWF021_45_defs.h
+source/HL_errata_SSWF021_45.obj: /home/marco/Tesis/microcontrolador/GM2804/include/HL_errata_SSWF021_45.h
+
+../source/HL_errata_SSWF021_45.c: 
+/home/marco/Tesis/microcontrolador/GM2804/include/HL_errata_SSWF021_45_defs.h: 
+/home/marco/Tesis/microcontrolador/GM2804/include/HL_errata_SSWF021_45.h: 
---
+++ microcontrolador/GM2804/Debug/source/os_mpu_wrappers.d
@@ -0,0 +1,51 @@
+# FIXED
+
+source/os_mpu_wrappers.obj: ../source/os_mpu_wrappers.c
+source/os_mpu_wrappers.obj: /home/marco/Tesis/microcontrolador/GM2804/include/FreeRTOS.h
+source/os_mpu_wrappers.obj: /home/marco/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stddef.h
+source/os_mpu_wrappers.obj: /home/marco/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_ti_config.h
+source/os_mpu_wrappers.obj: /home/marco/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/linkage.h
+source/os_mpu_wrappers.obj: /home/marco/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdint.h
+source/os_mpu_wrappers.obj: /home/marco/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_stdint40.h
+source/os_mpu_wrappers.obj: /home/marco/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/stdint.h
+source/os_mpu_wrappers.obj: /home/marco/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/cdefs.h
+source/os_mpu_wrappers.obj: /home/marco/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_types.h
+source/os_mpu_wrappers.obj: /home/marco/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_types.h
+source/os_mpu_wrappers.obj: /home/marco/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_stdint.h
+source/os_mpu_wrappers.obj: /home/marco/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_stdint.h
+source/os_mpu_wrappers.obj: /home/marco/Tesis/microcontrolador/GM2804/include/FreeRTOSConfig.h
+source/os_mpu_wrappers.obj: /home/marco/Tesis/microcontrolador/GM2804/include/os_projdefs.h
+source/os_mpu_wrappers.obj: /home/marco/Tesis/microcontrolador/GM2804/include/os_portable.h
+source/os_mpu_wrappers.obj: /home/marco/Tesis/microcontrolador/GM2804/include/os_portmacro.h
+source/os_mpu_wrappers.obj: /home/marco/Tesis/microcontrolador/GM2804/include/os_mpu_wrappers.h
+source/os_mpu_wrappers.obj: /home/marco/Tesis/microcontrolador/GM2804/include/os_task.h
+source/os_mpu_wrappers.obj: /home/marco/Tesis/microcontrolador/GM2804/include/os_list.h
+source/os_mpu_wrappers.obj: /home/marco/Tesis/microcontrolador/GM2804/include/os_queue.h
+source/os_mpu_wrappers.obj: /home/marco/Tesis/microcontrolador/GM2804/include/os_timer.h
+source/os_mpu_wrappers.obj: /home/marco/Tesis/microcontrolador/GM2804/include/os_list.h
+source/os_mpu_wrappers.obj: /home/marco/Tesis/microcontrolador/GM2804/include/os_event_groups.h
+
+../source/os_mpu_wrappers.c: 
+/home/marco/Tesis/microcontrolador/GM2804/include/FreeRTOS.h: 
+/home/marco/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stddef.h: 
+/home/marco/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_ti_config.h: 
+/home/marco/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/linkage.h: 
+/home/marco/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdint.h: 
+/home/marco/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_stdint40.h: 
+/home/marco/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/stdint.h: 
+/home/marco/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/cdefs.h: 
+/home/marco/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_types.h: 
+/home/marco/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_types.h: 
+/home/marco/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_stdint.h: 
+/home/marco/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_stdint.h: 
+/home/marco/Tesis/microcontrolador/GM2804/include/FreeRTOSConfig.h: 
+/home/marco/Tesis/microcontrolador/GM2804/include/os_projdefs.h: 
+/home/marco/Tesis/microcontrolador/GM2804/include/os_portable.h: 
+/home/marco/Tesis/microcontrolador/GM2804/include/os_portmacro.h: 
+/home/marco/Tesis/microcontrolador/GM2804/include/os_mpu_wrappers.h: 
+/home/marco/Tesis/microcontrolador/GM2804/include/os_task.h: 
+/home/marco/Tesis/microcontrolador/GM2804/include/os_list.h: 
+/home/marco/Tesis/microcontrolador/GM2804/include/os_queue.h: 
+/home/marco/Tesis/microcontrolador/GM2804/include/os_timer.h: 
+/home/marco/Tesis/microcontrolador/GM2804/include/os_list.h: 
+/home/marco/Tesis/microcontrolador/GM2804/include/os_event_groups.h: 
